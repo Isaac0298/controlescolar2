047a9b04cf5c019983d6064307236b23cf631933
eavch <eavch@DESKTOP-P78IQ0B>
2022-03-03 23:34:51 -0600

    Agrege clase principal

---
+++ src/main/java/mx/com/controlescolar2/main/Principal.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mx.com.controlescolar2.main;
+
+import mx.com.upiicsa.controlescolar2.Alumno;
+import mx.com.upiicsa.controlescolar2.Profesor;
+import mx.com.upiicsa.controlescolar2.academia.Curso;
+
+/**
+ *
+ * @author Gilberto Garcia
+ */
+public class Principal {
+    public static void main(String[] args) {
+        Alumno alumno1 = new Alumno();
+        Profesor profe = new Profesor();
+        Curso curso1 = new Curso();
+    }
+    
+}
